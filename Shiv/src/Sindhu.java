153131193a453783d0e7cfa45d1bc30a8e90156a
= <=>
2021-04-03 10:19:57 +0530

    new msg

---
+++ Shiv/src/Sindhu.java
@@ -0,0 +1,13 @@
+
+public class Sindhu {
+	public static void main(String[] args) {
+		int a[] = new int[]{1,5,8,9};
+		
+		for(int i=a.length-1;i>=0;i--)
+		{
+			System.out.println(a[i]);
+		}
+	}
+}
+
+
